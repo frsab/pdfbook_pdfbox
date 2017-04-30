0ee39d1cf3a265f540260bb28f322ef3ebffd201
Saber <frejsaber@gmail.com>
2017-05-01 00:38:47 +0200

    gitignore ressources folder

--- src/main/java/com/pdf/book/pdfbook/App.java
+++ src/main/java/com/pdf/book/pdfbook/App.java
@@ -1,5 +1,6 @@
 package com.pdf.book.pdfbook;
 
+import java.io.File;
 import java.util.Properties;
 
 /**
@@ -14,6 +15,23 @@ public class App
     {
         pl = new PropertyLoader();
         System.out.println(pl.getDefaultProps().getProperty("name"));
-        System.out.println(pl.getApplicationProps().getProperty("nom"));
+        System.out.println(pl.getApplicationProps().getProperty("inupt.folder"));
+        
+        String [] listefichiers; 
+//        File repertoire=new File(pl.getApplicationProps().getProperty("inupt.folder"));  
+        File repertoire=new File("src/ressources/EditionENI_UnivLyon");  
+        //EditionENI_UnivLyon
+        int i; 
+        listefichiers=repertoire.list(); 
+        for (String fichier : listefichiers) {
+			System.out.println(fichier);
+		}
+        for(i=0;i<listefichiers.length;i++){ 
+        if(listefichiers[i].endsWith(".java")==true){ 
+
+        System.out.println(listefichiers[i].substring(0,listefichiers[i].length()-5));// on choisit la sous chaine - les 5 derniers caracteres ".java" 
+        } 
+        } 
+        }
     }
-}
+
