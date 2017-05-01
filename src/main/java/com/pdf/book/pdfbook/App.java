79651b2e98824fd41ba3b014e1696809c357d3c9
Saber <frejsaber@gmail.com>
2017-05-01 12:18:33 +0200

    cacher le constantes locaux dans une classe Constante

--- src/main/java/com/pdf/book/pdfbook/App.java
+++ src/main/java/com/pdf/book/pdfbook/App.java
@@ -18,20 +18,15 @@ public class App
         System.out.println(pl.getApplicationProps().getProperty("inupt.folder"));
         
         String [] listefichiers; 
-//        File repertoire=new File(pl.getApplicationProps().getProperty("inupt.folder"));  
-        File repertoire=new File("src/ressources/EditionENI_UnivLyon");  
-        //EditionENI_UnivLyon
+        File repertoire=new File(Constante.DOSSIER_PERE);  
+
         int i; 
         listefichiers=repertoire.list(); 
         for (String fichier : listefichiers) {
 			System.out.println(fichier);
 		}
-        for(i=0;i<listefichiers.length;i++){ 
-        if(listefichiers[i].endsWith(".java")==true){ 
-
-        System.out.println(listefichiers[i].substring(0,listefichiers[i].length()-5));// on choisit la sous chaine - les 5 derniers caracteres ".java" 
-        } 
-        } 
-        }
+    
     }
+    
+}
 
